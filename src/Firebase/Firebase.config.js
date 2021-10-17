4853a6f8195b69d70ab23b103476d11be9239f57
sabbir <sabbirhossen58sh58gmail.com>
2021-10-18 01:15:18 +0600

    first commit i firebase

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC3qioW65pQU60mo4-QODPsKWG_JjL7nUk",
+    authDomain: "healthcare-assignment10.firebaseapp.com",
+    projectId: "healthcare-assignment10",
+    storageBucket: "healthcare-assignment10.appspot.com",
+    messagingSenderId: "283352265736",
+    appId: "1:283352265736:web:ff8de45809c49b8734cdbd"
+};
+export default firebaseConfig;
